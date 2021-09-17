9da308440d1bd56730b9ba61f5a136d101c26b9b
dimple5 <dimple@vernacular.ai>
2021-09-17 17:53:26 +0530

    inital commit

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -3,7 +3,6 @@ import { OrbitControls, Stars } from "@react-three/drei"
 import React, { useRef } from 'react';
 import EarthCloud from "../../assets/textures/8k_earth_clouds.jpg"
 import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg"
-import EarthNight from "../../assets/textures/8k_earth_nightmap.jpg"
 import EarthNormal from "../../assets/textures/8k_earth_normal_map.jpg"
 import EarthSpecular from "../../assets/textures/8k_earth_specular_map.jpg"
 import {TextureLoader} from "three"
@@ -20,20 +19,20 @@ const Earth = (props) => {
     return ( 
         <>
             {/* <ambientLight intensity={1}/>  */}
-            <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={1.2}/>
+            <pointLight color="#f6f3ea" position={[2, 0, 5]} intensity={1.2}/>
             <Stars radius={300} 
                 depth={60} 
                 count={20000} 
                 factor={7} 
                 saturation={0} 
                 fade={true}/>
-            <mesh ref={cloudRef}>
+            <mesh ref={cloudRef} position={[0 ,0, 3]}>
                 <sphereGeometry args={[1.005,32,32]} />
                 <meshPhongMaterial map={cloudMap} 
                 opacity={0.4} depthWrite={true} 
                 transparent={true} side={THREE.DoubleSide}/>
             </mesh>
-            <mesh ref={earthRef}>
+            <mesh ref={earthRef} position={[0 ,0, 3]}>
                 <sphereGeometry args={[1, 32, 32]}/>
                 <meshPhongMaterial specularMap={specularMap} />
                 <meshStandardMaterial map={colorMap} normalMap={normalMap} metalness={0.4} roughness={0.7}/>
