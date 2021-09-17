1eb376fcb515f7eb0ec8a9feb756be48c13b76f7
dimple5 <dimple@vernacular.ai>
2021-09-17 17:24:24 +0530

    initail commit

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,6 +1,6 @@
-import { useLoader } from '@react-three/fiber';
+import { useFrame, useLoader } from '@react-three/fiber';
 import { OrbitControls, Stars } from "@react-three/drei"
-import React from 'react';
+import React, { useRef } from 'react';
 import EarthCloud from "../../assets/textures/8k_earth_clouds.jpg"
 import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg"
 import EarthNight from "../../assets/textures/8k_earth_nightmap.jpg"
@@ -10,26 +10,33 @@ import {TextureLoader} from "three"
 import * as THREE from "three"
 const Earth = (props) => {
     const [colorMap,normalMap, specularMap, cloudMap] = useLoader(TextureLoader, [EarthDayMap, EarthNormal, EarthSpecular, EarthCloud])
+    const earthRef = useRef()
+    const cloudRef = useRef()
+    useFrame (({clock}) => {
+        const elapsedTime = clock.getElapsedTime()
+        earthRef.current.rotation.y = elapsedTime / 6;
+        cloudRef.current.rotation.y = elapsedTime / 6;
+    })
     return ( 
         <>
-            <ambientLight intensity={1}/> 
-            <pointLight color="#f6f3ea" position={[2,0,2]} intensity={1.2}/>
+            {/* <ambientLight intensity={1}/>  */}
+            <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={1.2}/>
             <Stars radius={300} 
                 depth={60} 
                 count={20000} 
                 factor={7} 
                 saturation={0} 
                 fade={true}/>
-            <mesh>
+            <mesh ref={cloudRef}>
                 <sphereGeometry args={[1.005,32,32]} />
                 <meshPhongMaterial map={cloudMap} 
                 opacity={0.4} depthWrite={true} 
                 transparent={true} side={THREE.DoubleSide}/>
             </mesh>
-            <mesh>
+            <mesh ref={earthRef}>
                 <sphereGeometry args={[1, 32, 32]}/>
                 <meshPhongMaterial specularMap={specularMap} />
-                <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+                <meshStandardMaterial map={colorMap} normalMap={normalMap} metalness={0.4} roughness={0.7}/>
                 <OrbitControls 
                     enabledZoom={true} 
                     enablePan={true} 
